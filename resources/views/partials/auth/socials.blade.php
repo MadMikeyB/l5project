60dce97d061625e34efe7577550d2a5430ada7c8
Michael <mikeybsaccount@gmail.com>
2016-02-21 02:13:44 +0000

    nicer socials partial

--- resources/views/partials/auth/socials.blade.php
+++ resources/views/partials/auth/socials.blade.php
@@ -1,12 +1,15 @@
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <a class="btn btn-block btn-social btn-github" href="{{ url('/social/github') }}"><i class="fa fa-github"></i>Log In With GitHub</a>
-                                <a class="btn btn-block btn-social btn-google" href="{{ url('/social/google') }}"><i class="fa fa-google"></i>Log In With Google</a>
-                                <a class="btn btn-block btn-social btn-linkedin" href="{{ url('/social/linkedin') }}"><i class="fa fa-linkedin"></i>Log In With LinkedIn</a>
-                                <a class="btn btn-block btn-social btn-facebook" href="{{ url('/social/facebook') }}"><i class="fa fa-facebook"></i>Log In With Facebook</a>
-                                <a class="btn btn-block btn-social btn-twitter" href="{{ url('/social/twitter') }}"><i class="fa fa-twitter"></i>Log In With Twitter</a>
-                                <a class="btn btn-block btn-social btn-tumblr" href="{{ url('/social/tumblr') }}"><i class="fa fa-tumblr"></i>Log In With Tumblr</a>
-                                <a class="btn btn-block btn-social btn-microsoft" href="{{ url('/social/microsoft') }}"><i class="fa fa-windows"></i>Log In With Microsoft</a>
-                            </div>
-                        </div>
+</div>
+<div class="panel-heading"><strong>Easy Mode:</strong> Connect With a Social Network</div>
+<div class="panel-body">
+<div class="col-md-6 col-md-offset-4">
+    <div class="btn-group" role="group" aria-label="...">
+        <a href="{{ url('/social/facebook') }}" type="button" class="btn btn-facebook"><i class="fa fa-facebook"></i></a>
+        <a href="{{ url('/social/twitter') }}" type="button" class="btn btn-twitter"><i class="fa fa-twitter"></i></a>
+        <a href="{{ url('/social/google') }}" type="button" class="btn btn-google"><i class="fa fa-google"></i></a>
+        {{-- <a href="{{ url('/social/tumblr') }}" type="button" class="btn btn-tumblr"><i class="fa fa-tumblr"></i></a> --}}
+        {{-- <a href="{{ url('/social/windows') }}" type="button" class="btn btn-microsoft"><i class="fa fa-windows"></i></a> --}}
+        <a href="{{ url('/social/linkedin') }}" type="button" class="btn btn-linkedin"><i class="fa fa-linkedin"></i></a>
+        <a href="{{ url('/social/bitbucket') }}" type="button" class="btn btn-bitbucket"><i class="fa fa-bitbucket"></i></a>
+    </div>
+</div>
+</div>
